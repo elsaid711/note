fc7f473f56e88b8c2f114070cdc93fd34954999e
elsaid711 <eltohfabela55@gmail.com>
2025-01-08 23:59:30 +0200

    show model buttom sheet (add note )

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:note/widgets/notes_view_body.dart';
 
+import '../widgets/add_note.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      floatingActionButton:FloatingActionButton(onPressed: (){},
+      floatingActionButton:FloatingActionButton(onPressed: (){
+        showModalBottomSheet(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          context: context, builder: (context) {
+          return AddNoteButtomSheet();
+        },);
+      },
       child: Icon(Icons.add),
       ) ,
       body:const notesViewBody(),
---
+++ lib/widgets/add_note.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+class AddNoteButtomSheet extends StatelessWidget {
+  const AddNoteButtomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextField(
+        
+      ),
+    );
+  }
+}
