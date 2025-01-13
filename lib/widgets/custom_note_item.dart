f79e06d796dad7bc517a1a1408610378135ef18c
elsaid711 <eltohfabela55@gmail.com>
2025-01-13 19:20:23 +0200

    create edite note view , Body

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
+
+import '../views/edit_note_view.dart';
 class noteItem extends StatelessWidget {
   const noteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 10,bottom: 10),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.orange,
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        },));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 10,bottom: 10),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.orange,
+          
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Text('Flutter Tips',style: TextStyle(color: Colors.black,fontSize: 26),),
+          ),
         
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-        title: Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: Text('Flutter Tips',style: TextStyle(color: Colors.black,fontSize: 26),),
+        subtitle: Text('Build your career with flutter',style: TextStyle(color: Colors.black.withOpacity(.5),fontSize: 18),),
+          trailing: IconButton(onPressed: (){}, icon: Icon( Icons.delete,color: Colors.black,size: 35,)),
+        ),
+          Padding(
+            padding: const EdgeInsets.only(right: 10,top: 10),
+            child: Text('Jan 08 2025',style: TextStyle(color: Colors.black.withOpacity(.5)),),
+          ),
+          ],
         ),
       
-      subtitle: Text('Build your career with flutter',style: TextStyle(color: Colors.black.withOpacity(.5),fontSize: 18),),
-        trailing: IconButton(onPressed: (){}, icon: Icon( Icons.delete,color: Colors.black,size: 35,)),
       ),
-        Padding(
-          padding: const EdgeInsets.only(right: 10,top: 10),
-          child: Text('Jan 08 2025',style: TextStyle(color: Colors.black.withOpacity(.5)),),
-        ),
-        ],
-      ),
-    
     );
 
   }
