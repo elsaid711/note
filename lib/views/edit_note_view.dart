772b12e61bf8890700d371977cc2df5d44d07958
elsaid711 <eltohfabela55@gmail.com>
2025-01-13 23:42:41 +0200

    Generate Type Adapter

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -7,7 +7,9 @@ class EditNoteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: EditNoteViewBody(),
+      body: SingleChildScrollView(
+        child: EditNoteViewBody(),
+      )
     );
   }
 }
